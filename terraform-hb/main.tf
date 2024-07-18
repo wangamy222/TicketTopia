0722cfe8cf19ba22405df106caddd56ecd824aab
0415bichan <0415bichan@gmail.com>
2024-07-18 16:54:45 +0900

    Update security group

--- terraform-hb/main.tf
+++ terraform-hb/main.tf
@@ -2,20 +2,22 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
-resource "aws_key_pair" "deployer" {
-  key_name   = "deployer-key"
-  public_key = 
-}
-
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web"
+  description = "Allow web inbound traffic"
 
   ingress {
-    from_port   = 22
-    to_port     = 22
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # 모든 IP 주소에서의 접근을 허용
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -30,9 +32,8 @@ resource "aws_instance" "example" {
   count         = 2
   ami           = "ami-056a29f2eddc40520"  # 우분투 22.04 ap-northeast-2
   instance_type = "t2.micro"
-  subnet_id     = "subnet-04e8b486d69d738fb"
-  key_name      = aws_key_pair.deployer.key_name
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  subnet_id     = "subnet-04e1ebc8f083b974b"
+  vpc_security_group_ids = [aws_security_group.allow_web.id]
   associate_public_ip_address = true
   tags = {
     Name = "example-instance-${count.index}"
