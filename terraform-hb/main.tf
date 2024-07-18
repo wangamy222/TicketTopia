7971882bc495bc6db37829cda03bb7b68fd618cd
0415bichan <0415bichan@gmail.com>
2024-07-18 17:43:25 +0900

    add code to create ALB

--- terraform-hb/main.tf
+++ terraform-hb/main.tf
@@ -2,6 +2,8 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
+
+# Security Group
 resource "aws_security_group" "allow_web" {
   name        = "allow_web"
   description = "Allow web inbound traffic"
@@ -28,6 +30,7 @@ resource "aws_security_group" "allow_web" {
   }
 }
 
+# EC2 instance
 resource "aws_instance" "example" {
   count         = 2
   ami           = "ami-056a29f2eddc40520"  # 우분투 22.04 ap-northeast-2
@@ -40,6 +43,7 @@ resource "aws_instance" "example" {
   }
 }
 
+# S3 bucket
 resource "aws_s3_bucket" "example_bucket" {
   bucket = "0718-bichan-test"
   
@@ -61,4 +65,79 @@ resource "aws_s3_bucket_acl" "example_bucket_acl" {
 
   bucket = aws_s3_bucket.example_bucket.id
   acl    = "private"
+}
+
+
+# Launch Template
+resource "aws_launch_template" "example" {
+  name_prefix   = "example-template"
+  image_id      = "ami-056a29f2eddc40520"  # 우분투 22.04 ap-northeast-2
+  instance_type = "t2.micro"
+
+  network_interfaces {
+    associate_public_ip_address = true
+    security_groups             = [aws_security_group.allow_web.id]
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "example-asg-instance"
+    }
+  }
+}
+
+
+resource "aws_autoscaling_group" "example" {
+  desired_capacity   = 2
+  max_size           = 5
+  min_size           = 1
+  target_group_arns  = [aws_lb_target_group.example.arn]
+  vpc_zone_identifier = ["subnet-04e8b486d69d738fb","subnet-04e1ebc8f083b974b"]
+
+  launch_template {
+    id      = aws_launch_template.example.id
+    version = "$Latest"
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "example-asg"
+    propagate_at_launch = true
+  }
+}
+
+# Load Balancer
+resource "aws_lb" "example" {
+  name               = "example-lb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.allow_web.id]
+  subnets            = ["subnet-04e8b486d69d738fb","subnet-04e1ebc8f083b974b"]
+
+  enable_deletion_protection = false
+}
+
+resource "aws_lb_listener" "example" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.example.arn
+  }
+}
+
+resource "aws_lb_target_group" "example" {
+  name     = "example-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "vpc-0061233d1a215be71"
+
+  health_check {
+    path                = "/"
+    healthy_threshold   = 2
+    unhealthy_threshold = 10
+  }
 }
