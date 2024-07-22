0e9d6fa801535b01b1205c29cd305c7f95604d32
왕지호 <97782317+wangamy222@users.noreply.github.com>
2024-07-23 00:54:25 +0900

    Update settings.py

--- tickettopia/tickettopia/settings.py
+++ tickettopia/tickettopia/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%ui&z%k1e3ong8sn3l)m10h6y7z%l+$wg1^w6_f_)3=-ra^a0a'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
