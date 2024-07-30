e53791e431d0a3597b0ea1232ebb54bdb160c54e
왕지호 <97782317+wangamy222@users.noreply.github.com>
2024-07-30 09:19:22 +0900

    Delete tickettopia directory

--- tickettopia/main/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class LoginForm(forms.Form):
-    uid = forms.CharField(max_length=50)
-    password = forms.CharField(widget=forms.PasswordInput)
--- tickettopia/main/templatetags/custom_filters.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def split_at(value, delimiter):
-    return value.split(delimiter)[0]
