129a85269d92ec0b20b99024716bc552fe4380a4
wangamy222 <wangamy@naver.com>
2024-07-30 13:07:45 +0900

    대기열 설정, 날짜 수정

--- tickettopia/main/urls.py
+++ tickettopia/main/urls.py
@@ -14,4 +14,8 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('create-payment/', views.create_payment, name='create_payment'),
+    path('join-queue/', views.join_queue, name='join_queue'),
+    path('check-queue-status/', views.check_queue_status,
+         name='check_queue_status'),
+    path('waiting-room/', views.waiting_room, name='waiting_room'),
 ]
